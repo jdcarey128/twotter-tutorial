73abffb298346e2e8da2b70ca0fdead8aebf9110
Joshua Carey <jdcarey128@gmail.com>
2021-03-26 11:05:26 -0600

    Refactor component styling

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,39 +1,36 @@
 <template>
   <div class="user-profile">
-    <div class="user-profile__user-panel">
-      <h1 class="user-profile__username">@{{ user.username }}</h1>
-      <div class="user-profile__admin-badge" v-if="user.isAdmin">
-        Admin
+    <div class="user-profile__sidebar">
+      <div class="user-profile__user-panel">
+        <h1 class="user-profile__username">@{{ user.username }}</h1>
+        <div class="user-profile__admin-badge" v-if="user.isAdmin">
+          Admin
+        </div>
+        <div class="user-profile__follower-count">
+          <strong>Followers: </strong> {{ followers }}
+        </div>
       </div>
-      <div class="user-profile__follower-count">
-        <strong>Followers: </strong> {{ followers }}
-      </div>
-      <div class="user-profile__create-twoot">
-        <NewTwootForm
-          :twootTypes="twootTypes"
-          @createNewTwoot="createNewTwoot"
+      <CreateTwootPanel @createNewTwoot="createNewTwoot"/>
+    </div>
+      <div class="user-profile__twoots-wrapper">
+        <TwootItem 
+          v-for="twoot in user.twoots" 
+          :key="twoot.id" 
+          :username="user.username" 
+          :twoot="twoot" 
+          @favorite="toggleFavorite"
         />
       </div>
-    </div>
-    <div class="user-profile__twoots-wrapper">
-      <TwootItem 
-        v-for="twoot in user.twoots" 
-        :key="twoot.id" 
-        :username="user.username" 
-        :twoot="twoot" 
-        @favorite="toggleFavorite"
-      />
-    </div>
   </div>
 </template>
 
 <script>
 import TwootItem from './TwootItem.vue';
-import NewTwootForm from './NewTwootForm.vue';
+import CreateTwootPanel from './CreateTwootPanel.vue';
 
 export default {
   name: 'UserProfile',
-  components: { TwootItem, NewTwootForm },
+  components: { TwootItem, CreateTwootPanel },
   data() {
     return {
       followers: 0,
@@ -51,18 +48,18 @@ export default {
       }
     }
   }, 
-  watch: {
-    followers(newFollowerCount, oldFollowerCount) {
-      if (oldFollowerCount < newFollowerCount) {
-        console.log(`${this.user.username} has gained a follower!`);
-      }
-    }
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
-    }
-  }, 
+  // watch: {
+  //   followers(newFollowerCount, oldFollowerCount) {
+  //     if (oldFollowerCount < newFollowerCount) {
+  //       console.log(`${this.user.username} has gained a follower!`);
+  //     }
+  //   }
+  // },
+  // computed: {
+  //   fullName() {
+  //     return `${this.user.firstName} ${this.user.lastName}`;
+  //   },
+  // }, 
   methods: {
     followUser() {
       this.followers ++
@@ -84,38 +81,48 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .user-profile {
   display: grid;
   grid-template-columns: 1fr 3fr;
   grid-gap: 50px;
   padding: 50px 5%;
+
+  .user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    background-color: white;
+    border-radius: 10px;
+    border: 1px solid #DFE3E8;
+    margin-bottom: auto;
+
+    h1 {
+      margin: 0;
+    }
+
+    .user-profile__admin-badge {
+      background: rebeccapurple;
+      color: white;
+      border-radius: 5px;
+      margin-right: auto;
+      padding: 0 10px; 
+      font-weight: bold;
+    }
+
+    .user-profile__twoots-wrapper {
+      display: grid;
+      grid-gap: 10px;
+      margin-bottom: auto;
+    }
+  }
 }
 
-.user-profile__user-panel {
-  display: flex;
-  flex-direction: column;
-  padding: 20px;
-  background-color: white;
-  border-radius: 10px;
-  border: 1px solid #DFE3E8
-}
 
-h1 {
-  margin: 0;
-}
 
-.user-profile__admin-badge {
-  background: rebeccapurple;
-  color: white;
-  border-radius: 5px;
-  margin-right: auto;
-  padding: 0 10px; 
-  font-weight: bold;
-}
 
-.user-profile__twoots-wrapper {
-  display: grid;
-  grid-gap: 10px;
-}
+
+
+
+
 </style>
